008414ddcdcb940b71a922497ebd1e774c2af397
Alex Eng <aeng@redhat.com>
2017-05-08 13:19:22 +1000

    fix: handle multiple html node in large string

--- src/main/java/org/zanata/mt/service/PersistentTranslationService.java
+++ src/main/java/org/zanata/mt/service/PersistentTranslationService.java
@@ -5,7 +5,6 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import javax.annotation.Nonnull;
 import javax.ejb.TransactionAttribute;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
--- src/test/java/org/zanata/mt/util/ArticleUtilTest.java
+++ src/test/java/org/zanata/mt/util/ArticleUtilTest.java
@@ -47,7 +47,7 @@ public class ArticleUtilTest {
     @Test
     public void asElement() {
         String html = "<html><body>test</body></html>";
-        Node node = ArticleUtil.asNode(html);
+        Node node = ArticleUtil.asElement(html);
         assertThat(node).isNotNull().extracting(Node::outerHtml)
                 .contains(html);
     }
@@ -72,33 +72,24 @@ public class ArticleUtilTest {
     @Test
     public void replaceNonTranslatableNode() {
         String node1 = "<span translate=\"no\">do not translate</span>";
-        String node2 = "<pre>do not translate</pre>";
-        String node3 = "<em>do not translate</em>";
+        String node2 = "<em>translate</em>";
         String node4 = "<span class=\"notranslate\">do not translate</span>";
         String node5 = "<span id=\"private-notes-testing\">do not translate</span>";
         String node6 = "<span>translate this</span>";
         String node7 = "<p>translate this</p>";
-        String html = "<div>" + node1 + node2 + node3 + node4 + node5 + node6 +
+        String html = "<div>" + node1 + node2 + node4 + node5 + node6 +
                 node7 + "</div>";
         TranslatableHTMLNode
                 node = ArticleUtil.replaceNonTranslatableNode(1, html);
-        assertThat(node.getPlaceholderIdMap()).hasSize(5);
+        assertThat(node.getPlaceholderIdMap()).hasSize(3);
         assertThat(node.getPlaceholderIdMap().values()).extracting(
                 Node::toString)
-                .contains(node1, node2, node3, node4, node5)
+                .contains(node1, node4, node5)
                 .doesNotContain(node6, node7);
-        assertThat(node.getHtml()).doesNotContain(node1).doesNotContain(node2)
-                .doesNotContain(node3).doesNotContain(node4)
-                .doesNotContain(node5).contains(node6, node7);
+        assertThat(node.getHtml()).doesNotContain(node1)
+                .doesNotContain(node4)
+                .doesNotContain(node5).contains(node2, node6, node7);
     }
-
-    @Test
-    public void test() {
-        String html = "<code class=\"notranslate\">The ski is blue.</code><p>This is a test of what kinds of fruit that you like best.  Oranges? Grapes?</p>";
-        TranslatableHTMLNode node = ArticleUtil.replaceNonTranslatableNode(1, html);
-        System.out.println(node.getHtml());
-    }
-
 
     @Test
     public void replacePlaceholderWithNode() {
