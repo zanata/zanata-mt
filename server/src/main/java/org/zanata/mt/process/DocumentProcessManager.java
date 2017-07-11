c13009e317bfa0870fd9a46f33db84a762b846ec
Alex Eng <aeng@redhat.com>
2017-07-11 10:12:33 +1000

    feat: limit concurrency processing on document key
    https://zanata.atlassian.net/browse/ZNTAMT-3

--- server/src/main/java/org/zanata/mt/process/DocumentProcessManager.java
+++ server/src/main/java/org/zanata/mt/process/DocumentProcessManager.java
@@ -1,23 +1,26 @@
 package org.zanata.mt.process;
 
-import org.infinispan.Cache;
-import org.infinispan.manager.DefaultCacheManager;
+import com.google.common.annotations.VisibleForTesting;
+import org.infinispan.util.concurrent.locks.StripedLock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.zanata.mt.api.dto.DocumentContent;
 import org.zanata.mt.api.dto.LocaleCode;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
 import javax.validation.constraints.NotNull;
-import java.util.concurrent.locks.ReentrantLock;
 
 /**
+ * Important: This lock uses single thread lock library which does not support
+ * clustering.
+ *
  * Handle locking of api request for
  * {@link org.zanata.mt.api.service.DocumentResource#translate(DocumentContent, LocaleCode)}
  *
- * Limit to process one translation request at a time.
- * This lock uses single thread lock library which does not support clustering.
+ * Limit to process a {document + source language + target language}
+ * translation request at a time. See {@link DocumentProcessKey}
+ *
+ * TODO: Implement clustering lock
  *
  * @author Alex Eng <a href="mailto:aeng@redhat.com">aeng@redhat.com</a>
  */
@@ -26,42 +29,28 @@ public class DocumentProcessManager {
     private static final Logger LOG =
             LoggerFactory.getLogger(DocumentProcessManager.class);
 
-    protected static final String DOC_LOCK_CACHE = "DOCUMENT_LOCK_CACHE";
-
-    private DefaultCacheManager cacheManager;
-
-    private Cache<DocumentProcessKey, ReentrantLock> documentProcessCache;
+    private final StripedLock lock = new StripedLock();
 
     @SuppressWarnings("unused")
     public DocumentProcessManager() {
-    }
-
-    @Inject
-    public DocumentProcessManager(DefaultCacheManager cacheManager) {
-        this.cacheManager = cacheManager;
-        documentProcessCache = this.cacheManager.getCache(DOC_LOCK_CACHE);
     }
 
     public void lock(@NotNull DocumentProcessKey key) {
         if (LOG.isDebugEnabled()) {
             LOG.debug("Locking document translation request:{}", key.toString());
         }
-        documentProcessCache.putIfAbsent(key, new ReentrantLock(true));
-        documentProcessCache.get(key).lock();
+        lock.acquireLock(key, true);
     }
 
     public void unlock(@NotNull DocumentProcessKey key) {
         if (LOG.isDebugEnabled()) {
             LOG.debug("release document translation lock:{}", key.toString());
         }
-        if (documentProcessCache.containsKey(key)) {
-            documentProcessCache.get(key).unlock();
-            documentProcessCache.remove(key);
-        }
+        lock.releaseLock(key);
     }
 
-    public boolean isLocked(@NotNull DocumentProcessKey key) {
-        return documentProcessCache.containsKey(key) &&
-                documentProcessCache.get(key).isLocked();
+    @VisibleForTesting
+    protected int getTotalLockCount() {
+        return lock.getTotalLockCount();
     }
 }
