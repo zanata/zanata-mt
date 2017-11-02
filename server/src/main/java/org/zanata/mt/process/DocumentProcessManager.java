c2b7e2d26e2cbac52bcc15865a1384762a43aa70
Alex Eng <aeng@redhat.com>
2017-11-02 12:30:51 +1000

    WIP: infinispan lock for doc process

--- server/src/main/java/org/zanata/mt/process/DocumentProcessManager.java
+++ server/src/main/java/org/zanata/mt/process/DocumentProcessManager.java
@@ -1,13 +1,32 @@
 package org.zanata.mt.process;
 
 import com.google.common.annotations.VisibleForTesting;
+import org.infinispan.Cache;
+import org.infinispan.configuration.cache.CacheMode;
+import org.infinispan.configuration.cache.Configuration;
+import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.configuration.global.GlobalConfiguration;
+import org.infinispan.configuration.global.GlobalConfigurationBuilder;
+import org.infinispan.eviction.EvictionStrategy;
+import org.infinispan.eviction.EvictionType;
+import org.infinispan.manager.DefaultCacheManager;
+import org.infinispan.manager.EmbeddedCacheManager;
 import org.infinispan.util.concurrent.locks.StripedLock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.zanata.mt.api.dto.DocumentContent;
 import org.zanata.mt.api.dto.LocaleCode;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
+import javax.transaction.HeuristicMixedException;
+import javax.transaction.HeuristicRollbackException;
+import javax.transaction.NotSupportedException;
+import javax.transaction.RollbackException;
+import javax.transaction.SystemException;
+import javax.transaction.TransactionManager;
 import javax.validation.constraints.NotNull;
 import javax.ws.rs.core.Response;
 import java.util.function.Supplier;
@@ -30,31 +49,55 @@ public class DocumentProcessManager {
     private static final Logger LOG =
             LoggerFactory.getLogger(DocumentProcessManager.class);
 
-    private final StripedLock lock = new StripedLock();
+//    private final StripedLock lock = new StripedLock();
+
+    @Inject
+    private EmbeddedCacheManager cacheManager;
+
+    private Cache<DocumentProcessKey, Boolean> docProcessCache;
+
+    private TransactionManager tm;
 
     @SuppressWarnings("unused")
     public DocumentProcessManager() {
     }
 
-    private void lock(@NotNull DocumentProcessKey key) {
+    @PostConstruct
+    public void init() {
+        docProcessCache = cacheManager.getCache("docProcessCache");
+        tm = docProcessCache.getAdvancedCache().getTransactionManager();
+    }
+
+    private void lock(@NotNull DocumentProcessKey key)
+            throws SystemException, NotSupportedException,
+            HeuristicRollbackException, HeuristicMixedException,
+            RollbackException {
         if (LOG.isDebugEnabled()) {
             LOG.debug("Locking document translation request:{}", key.toString());
         }
-        lock.acquireLock(key, true);
+        tm.begin();
+        docProcessCache.getAdvancedCache().lock(key);
+        docProcessCache.put(key, true);
+        tm.commit();
+//        lock.acquireLock(key, true);
     }
 
     private void unlock(@NotNull DocumentProcessKey key) {
         if (LOG.isDebugEnabled()) {
             LOG.debug("release document translation lock:{}", key.toString());
         }
-        lock.releaseLock(key);
+        docProcessCache.remove(key);
+//        lock.releaseLock(key);
     }
 
     public Response withLock(DocumentProcessKey key,
             Supplier<Response> function) {
-        lock(key);
         try {
+            lock(key);
             return function.get();
+        } catch (Exception e) {
+            LOG.error("Unable to lock request process:" + key);
+            return Response.serverError().build();
         } finally {
             unlock(key);
         }
@@ -62,6 +105,25 @@ public class DocumentProcessManager {
 
     @VisibleForTesting
     protected int getTotalLockCount() {
-        return lock.getTotalLockCount();
+        return docProcessCache.size();
+//        lock.getTotalLockCount();
+    }
+
+    @Produces
+    @ApplicationScoped
+    public EmbeddedCacheManager defaultClusteredCacheManager() {
+        GlobalConfiguration g = new GlobalConfigurationBuilder()
+                .clusteredDefault()
+                .transport()
+                .clusterName("MachineTranslationsCluster")
+                .build();
+        Configuration cfg = new ConfigurationBuilder()
+                .clustering()
+                .cacheMode(CacheMode.DIST_ASYNC)
+                .eviction()
+                .strategy(EvictionStrategy.LRU)
+                .type(EvictionType.COUNT).size(150)
+                .build();
+        return new DefaultCacheManager(g, cfg);
     }
 }
