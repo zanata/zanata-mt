b6bbcb17fe95e7e88e480d08614ece3f529a79a5
kgough <kgough@redhat.com>
2018-08-17 16:00:47 +1000

    feat(ZNTAMT-48) WIP: TransUnit styling

--- frontend/src/app/components/SourceTrans/index.tsx
+++ frontend/src/app/components/SourceTrans/index.tsx
@@ -4,7 +4,9 @@ import 'antd/lib/layout/style/css';
 
 export class SourceTrans extends React.Component<{}> {
     public render() {
-        const sourceText = 'Good morning'
+        const sourceText = 'Guten morgen, Guten morgen Guten morgenGuten morgenGuten morgenGuten morgenGuten morgen' +
+                'Guten morgenGuten morgenGuten morgenGuten morgenGuten' +
+                ' morgenGuten morgenGuten morgen. Guten morgen!'
         return (
                 <Layout className="sourceTrans">
                     <span className="transWrapper">
